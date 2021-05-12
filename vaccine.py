c60b8b0801e1709e61f7250a5b528ea84667a5b8
Rahinpatel <rahinpatel1997@gmail.com>
2021-05-12 19:55:24 +0530

    Add files via upload

--- vaccine.py
+++ vaccine.py
@@ -5,6 +5,7 @@ from time import time, sleep
 
 parser = argparse.ArgumentParser(description='A test program.')
 parser.add_argument("--zipcode", help="Please give zip code.")
+parser.add_argument("--age", help="Please give your age.")
 
 headers = {
     'accept': 'application/json',
@@ -13,7 +14,8 @@ headers = {
 }
 
 
-def api_call(zipcode, today):
+def api_call(zipcode, today, age):
+    print(age)
     try:
         result = requests.get(('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin'), headers=headers, params={
             'pincode': zipcode,
@@ -22,17 +24,25 @@ def api_call(zipcode, today):
         if result.status_code == 200:
             json_response = result.json()
             data = json_response['sessions']
-            print(data)
+            validSlots = [key for key in data if ((key["available_capacity"])
+                          > 0 and (key["min_age_limit"]<= int(age)))]
+            notifyMe(validSlots)
             return None
     except:
         print("something went wrong pls try again")
 
 
+def notifyMe(validSlots):
+    print(validSlots, "validSlots")
+    pass
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
     zipcode = args.zipcode
+    age = args.age
     today = date.today().strftime('%d-%m-%Y')
     print("Finding..... Near by Vaccine")
     # print(sleep(60 - time() % 60))
     # sleep(60 - time() % 60)
-    api_call(zipcode, today)
+    api_call(zipcode, today, age)
