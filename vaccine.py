3c2f0f84952e99be823b6d23b8650ee347fb8ecb
Rahinpatel <rahinpatel1997@gmail.com>
2021-05-13 19:18:41 +0530

    Add files via upload

--- vaccine.py
+++ vaccine.py
@@ -1,10 +1,20 @@
+import os
 import requests
 import argparse
-from datetime import date
+import slack
+from pathlib import Path
+from dotenv import load_dotenv
+from datetime import date, timedelta
 import time
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 parser = argparse.ArgumentParser(description='A test program.')
 parser.add_argument("--zipcode", help="Please give zip code.")
 parser.add_argument("--age", help="Please give your age.")
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
 headers = {
     'accept': 'application/json',
@@ -17,6 +27,7 @@ def api_call(zipcode, today, age):
     count = 1
     try:
         while (count < 10000000000):
+            print("Try",count)
             result = requests.get(('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin'), headers=headers, params={
                 'pincode': zipcode,
                 'date': today,
@@ -26,7 +37,8 @@ def api_call(zipcode, today, age):
                 data = json_response['sessions']
                 validSlots = [key for key in data if ((key["available_capacity"])
                                                       > 0 and (key["min_age_limit"] <= int(age)))]
-                notifyMe(validSlots)
+                if(len(validSlots) > 0):
+                    notifyMe(validSlots)
             else:
                 print("Server Down")
             count = count + 1
@@ -37,6 +49,7 @@ def api_call(zipcode, today, age):
 
 
 def notifyMe(validSlots):
+    client.chat_postMessage(channel='#test',text=validSlots)
     print(validSlots, "validSlots")
     pass
 
@@ -45,8 +58,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     zipcode = args.zipcode
     age = args.age
-    today = date.today().strftime('%d-%m-%Y')
+    today = date.today() + timedelta(1)
+    dt = today.strftime('%d-%m-%Y')
     print("Finding..... Near by Vaccine")
     # print(sleep(60 - time() % 60))
     # sleep(60 - time() % 60)
-    api_call(zipcode, today, age)
+    api_call(zipcode, dt, age)
